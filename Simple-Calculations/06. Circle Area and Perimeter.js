c608a23af7e7473283457478fc26026728777a0a
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 02:45:46 +0300

    Create 06. Circle Area and Perimeter.js

---
+++ Simple-Calculations/06. Circle Area and Perimeter.js
@@ -0,0 +1,7 @@
+function perimeterAreaCircel ([radius]){
+    let r = radius;
+    let area = Math.PI * r * r;
+    let perimeter = 2 * Math.PI * r;
+    console.log("Area = " + area);
+    console.log("Perimeter = " + perimeter);
+}
