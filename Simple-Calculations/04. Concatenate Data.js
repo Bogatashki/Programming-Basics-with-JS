9493e5aedd0b2045c6ef1102cd8e9b8a68603a5e
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 02:44:18 +0300

    Create 04. Concatenate Data.js

---
+++ Simple-Calculations/04. Concatenate Data.js
@@ -0,0 +1,4 @@
+function concatenateData([firstName, lastName, age, town]){
+    let result = "You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".";
+    console.log(result);
+}
