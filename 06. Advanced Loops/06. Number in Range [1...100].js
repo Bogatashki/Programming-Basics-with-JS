daae302992b1276b640a432135841f85dfe658f1
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 23:56:21 +0300

    Create 06. Number in Range [1...100].js

---
+++ 06. Advanced Loops/06. Number in Range [1...100].js
@@ -0,0 +1,10 @@
+function numberInRange(args) {
+    let i = 0;
+    let num = Number(args[i]);
+    while (num < 1 || num > 100) {
+        i++;
+        console.log("Invalid number!");
+        num = Number(args[i]);
+    }
+    console.log(`The number is: ${num}`);
+}
