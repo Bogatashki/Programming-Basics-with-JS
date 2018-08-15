6c9dd0893ab24a50f223fc9e90c701cc7c396741
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 12:55:38 +0300

    Create 02. Excellent or Not.js

---
+++ 02. Simple Conditional Statements/02. Excellent or Not.js
@@ -0,0 +1,9 @@
+function check ([evaluation]){
+    evaluation = Number(evaluation);
+    if (evaluation >= 5.50){
+        console.log("Excellent!")
+    }
+    else{
+        console.log("Not excellent.")
+    }
+}
