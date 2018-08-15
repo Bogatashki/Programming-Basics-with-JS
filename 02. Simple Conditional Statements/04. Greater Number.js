08ffb1fa29354f646fb1547b82f8375ac5de9571
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 12:59:15 +0300

    Create 04. Greater Number.js

---
+++ 02. Simple Conditional Statements/04. Greater Number.js
@@ -0,0 +1,10 @@
+function greaterNumber ([arg1, arg2]) {
+    arg1 = Number(arg1);
+    arg2 = Number(arg2);
+    if(arg1 > arg2) {
+        console.log(arg1);
+    }
+    else{
+        console.log(arg2);
+    }
+}
