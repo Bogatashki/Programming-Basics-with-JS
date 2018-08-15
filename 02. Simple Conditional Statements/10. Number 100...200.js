a4b901ade44c3ea6002c7e2292e4a2984fe2a722
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 17:46:03 +0300

    Create 10. Number 100...200.js

---
+++ 02. Simple Conditional Statements/10. Number 100...200.js
@@ -0,0 +1,10 @@
+function number100200 ([arg1]){
+    arg1 = Number(arg1);
+    if (arg1 < 100){
+        console.log("Less than 100");
+    } else if (arg1 > 200){
+        console.log("Greater than 200");
+    }else {
+        console.log("Between 100 and 200");
+    }
+}
