e9a163100d6e619bf7c11712c0105e596548baf7
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 22:14:45 +0300

    Create 04. Fruit or Vegetable.js

---
+++ 03. Complex Conditional Statements/04. Fruit or Vegetable.js
@@ -0,0 +1,9 @@
+function fruitOrVegetable ([arg1]){
+     if (arg1 == "banana" || arg1 == "apple" || arg1 == "kiwi" || arg1 == "cherry" || arg1 == "lemon" || arg1 == "grapes"){
+         console.log("fruit");
+     }else if (arg1 == "tomato" || arg1 == "cucumber" || arg1 == "pepper" || arg1 == "carrot"){
+        console.log("vegetable");
+     }else {
+        console.log("unknown");
+     }
+}
