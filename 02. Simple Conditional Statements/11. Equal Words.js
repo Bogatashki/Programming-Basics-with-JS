7e547fd69d84ff9bfec491b05ad0f196e5d89eae
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 17:47:59 +0300

    Create 11. Equal Words.js

---
+++ 02. Simple Conditional Statements/11. Equal Words.js
@@ -0,0 +1,9 @@
+function equalWords ([arg1, arg2]){
+   let upperCaseArg1 = arg1.toUpperCase();
+   let upperCaseArg2 = arg2.toUpperCase();
+   if (upperCaseArg1 == upperCaseArg2 ){
+        console.log("yes");
+   }else {
+       console.log("no");
+   }
+}
