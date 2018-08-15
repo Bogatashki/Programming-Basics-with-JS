881089ac3243d62ffa5fcf1f3d67ae65e06e88c5
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 17:40:15 +0300

    Create Number 1...9 to text.js

---
+++ 02. Simple Conditional Statements/Number 1...9 to text.js
@@ -0,0 +1,25 @@
+function numbertoText ([arg1]){
+    arg1 = Number(arg1);
+    if (arg1 == 1){
+        console.log("one");
+    }    else if (arg1 == 2){
+        console.log("two")
+    }   else if (arg1 == 3){
+        console.log("three")
+    }     else if (arg1 == 4){
+        console.log("four")
+    }     else if (arg1 == 5){
+        console.log("five")
+    }     else if (arg1 == 6){
+        console.log("six")
+    }     else if (arg1 == 7){
+        console.log("seven")
+    }     else if (arg1 == 8){
+        console.log("eight")
+    }     else if (arg1 == 9){
+        console.log("nine")
+    }     
+        else {
+            console.log("number too big")
+        }     
+}
