f55ab14137e8bc800d6ab7628dcf9ade301b9d95
Kostadin Spasov <41234043+Bogatashki@users.noreply.github.com>
2018-08-15 17:52:34 +0300

    Create 15. 3 Equal Numbers.js

---
+++ 02. Simple Conditional Statements/15. 3 Equal Numbers.js
@@ -0,0 +1,10 @@
+function equalNumbers ([arg1, arg2, arg3]){
+    arg1 = Number(arg1);
+    arg2 = Number(arg2);
+    arg3 = Number(arg3);
+    if (arg1 == arg2 && arg1 == arg3){
+        console.log("yes");
+    }else {
+        console.log("no");
+    }
+}
